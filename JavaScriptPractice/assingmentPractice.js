25826c6c26a1066a358c3bce3e8c3c2bf1bad1b1
Sandip <sandipgavali1446@gail.com>
2026-07-03 19:31:15 +0530

    added new files

---
+++ JavaScriptPractice/assingmentPractice.js
@@ -0,0 +1,5 @@
+//1. write a program to find the sum of two numbers using let variable
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log("The sum of", num1, "and", num2, "is:", sum); // Output: The sum of 5 and 10 is: 15
